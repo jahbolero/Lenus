2a54b26f937d46225c8930a386ead54526a8534b
Jeremiah Valero <Jeremiah.Valero@skanlog.com>
2019-10-31 11:44:33 +0800

    First Commit

---
+++ sample/Controllers/ValuesController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using sample.Models;
+using System.Web.Http.Cors;
+
+namespace sample.Controllers
+{
+    [RoutePrefix("api/values")]
+    public class StringTable
+    {
+        public string[] ColumnNames { get; set; }
+        public string[,] Values { get; set; }
+    }
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class ValuesController : ApiController
+    {
+
+        static async Task<string> InvokeRequestResponseService(Parameters details)
+        {
+            using (var client = new HttpClient())
+            {
+                var scoreRequest = new
+                {
+
+                    Inputs = new Dictionary<string, StringTable>() {
+                        {
+                            "input1",
+                            new StringTable()
+                            {
+                                ColumnNames = new string[] {"gender", "age", "currentSmoker", "cigsPerDay", "BPMeds", "prevalentHyp", "diabetes", "totChol", "sysBP", "diaBP", "BMI", "heartRate", "glucose", "TenYearCHD"},
+                                Values = new string[,] {  { details.gender, details.age, details.currentSmoker, details.cigsPerDay, details.BPMeds, details.prevalentHyp, details.diabetes, details.totChol, details.sysBP, details.diaBP, details.BMI, details.heartRate, details.glucose, "0" },  }
+                            }
+                        },
+                    },
+                    GlobalParameters = new Dictionary<string, string>()
+                    {
+                    }
+                };
+                const string apiKey = "qT/zfKa8n340N2GNDINp2brLnwmQGcJMLs7pnpLZLyw4W5gy+bMoKHV5tLDewFqrNVTj1c+GiJqQMBCwExxY8w=="; // Replace this with the API key for the web service
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+
+                client.BaseAddress = new Uri("https://ussouthcentral.services.azureml.net/workspaces/3ea4b54c3ccb46d4a795aa0bb2f06a99/services/bbc286f434e940e4886fa96bb1ab2423/execute?api-version=2.0&details=true");
+
+                // WARNING: The 'await' statement below can result in a deadlock if you are calling this code from the UI thread of an ASP.Net application.
+                // One way to address this would be to call ConfigureAwait(false) so that the execution does not attempt to resume on the original context.
+                // For instance, replace code such as:
+                //      result = await DoSomeTask()
+                // with the following:
+                //      result = await DoSomeTask().ConfigureAwait(false)
+
+
+                HttpResponseMessage response = await client.PostAsJsonAsync("", scoreRequest).ConfigureAwait(false);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine("Result: {0}", result);
+                    return result;
+
+                }
+                else
+                {
+                    Console.WriteLine(string.Format("The request failed with status code: {0}", response.StatusCode));
+
+                    // Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+                    Console.WriteLine(response.Headers.ToString());
+
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(responseContent);
+                    return responseContent;
+                }
+            }
+        }
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/val
+
+        // POST api/values
+        public HttpResponseMessage Post(Parameters value)
+        {
+            Task<string> task = InvokeRequestResponseService(value);
+            task.Wait();
+            var result = task.Result;
+            JObject json = JObject.Parse(result);
+            return Request.CreateResponse(HttpStatusCode.OK, json);
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
+
+
